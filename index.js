fb99d4bfbcdd19269d5a7e86d8a358a343b69d71
Jasmine Lok <jasminellok@gmail.com>
2020-05-10 22:21:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -87,15 +87,20 @@ function moveDodger(e) {
   }
 }
 
-//This function should move DODGER to the left, (mabye 4 pixels?). Use window.requestAnimationFrame()!
 function moveDodgerLeft() {
+  //This function should move DODGER to the left, (mabye 4 pixels?). Use window.requestAnimationFrame()!
+  window.requestAnimationFrame(function() {
+    const left =positionToInteger(DODGER.style.left)
+    if (left>0) {
+      DODGER.style.left = `${left-4}px`
+    }
+  });
 }
 
 
 function moveDodgerRight() {
   //This function should move DODGER to the right (mabye 4 pixels?). Use window.requestAnimationFrame()!
   window.requestAnimationFrame(function() {
-    console.log('going right')
     const left =positionToInteger(DODGER.style.left)
     if (left<360) {
       DODGER.style.left = `${left+4}px`
